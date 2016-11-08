5bb952e14f1173406357a0bc872e9c01e8a9efa6
cai1449074828 <1449074828@qq.com>
2016-11-08 16:09:52 +0800

    2.2
    2.2

--- mylibrary/src/main/java/com/blq/zzc/mylibrary/base/LazyFragment.java
+++ mylibrary/src/main/java/com/blq/zzc/mylibrary/base/LazyFragment.java
@@ -23,29 +23,24 @@ public abstract class LazyFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-
         rootView = inflater.inflate(getLayoutId(), container, false);
-
         return rootView;
     }
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
     {
-
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
         initViews();
     }
-
     @Override
     public void onDetach()
     {
-
         super.onDetach();
     }
-
     public abstract int getLayoutId();
-
     public abstract void initViews();
+    public View getRootView() {
+        return rootView;
+    }
 }
